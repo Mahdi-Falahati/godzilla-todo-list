85b49a7261c9cd3d02e9ca830807e147b868f0c1
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-04-04 23:10:26 -0700

    Complete popup for adding member

---
+++ src/Components/PopUpForm.jsx
@@ -0,0 +1,88 @@
+import {
+    Box,
+    Slide,
+    Button,
+    Dialog,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+  } from "@mui/material";
+  import CloseIcon from "@mui/icons-material/Close";
+  import GroupAddIcon from "@mui/icons-material/GroupAdd";
+  import HowToRegIcon from "@mui/icons-material/HowToReg";
+  
+  import { useState, forwardRef } from "react";
+  
+  const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+ 
+  export default function PopUpForm({ children, transferCloseData }) {
+    const [open, setOpen] = useState(false);
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    const saveCloseHandler = () => {
+      if (transferCloseData) {
+        setOpen(false);
+      }
+    };
+  
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={handleClickOpen}
+          startIcon={<GroupAddIcon />}
+        >
+          Add Member
+        </Button>
+        <Dialog
+          fullScreen
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Transition}
+        >
+          <AppBar sx={{ position: "relative", background: "#533391" }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleClose}
+                aria-label="close"
+              >
+                <CloseIcon />
+              </IconButton>
+              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                Add Member
+              </Typography>
+              <Button
+                endIcon={<HowToRegIcon />}
+                autoFocus
+                color="inherit"
+                onClick={saveCloseHandler}
+              >
+                Save
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Box sx={{ marginTop: "10px", height: "100%" }}>{children}</Box>
+        </Dialog>
+      </div>
+    );
+  }
+  
