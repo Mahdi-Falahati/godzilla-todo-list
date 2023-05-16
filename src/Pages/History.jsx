9cc2af1e130fabb16f753af8972dcc1602c15e6a
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-05-16 09:25:24 -0700

    Add fake data for show history component

--- src/Pages/History.jsx
+++ src/Pages/History.jsx
@@ -3,7 +3,12 @@ import HistroyOutput from "../Components/HistroyOutput";
 export default function History() {
   return (
     <div>
-      <HistroyOutput />
+      <HistroyOutput
+        TaskId="TaskID"
+        dataTime="2023 - 5 - 16"
+        userName="RC"
+        TypeOfModifiction="Type Of Modification"
+      />
     </div>
   );
 }
